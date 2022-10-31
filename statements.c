c8a2dfc9e779b9234321dc9e15b97ad975687f03
ghost code <116631167+viswaagz@users.noreply.github.com>
2022-10-31 18:19:25 +0530

    Add files via upload

---
+++ statements.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#define data 10
+int main()
+{
+	/* if(data==10)	//if statement
+	{
+		printf("if have running");
+	} */
+
+//------------------------------
+	
+   /* if (data==11) //if-else statement
+    {
+	printf("if part");
+	}
+	else
+	   {
+		printf("else part");
+		}	
+	*/
+//------------------------------	
+      
+	/* if(data==108)      //Nested If-else statement
+     {
+      if(data==19)
+       {
+ 		printf("its work on the filter layer");
+ 		}
+ 	  else 
+ 		{
+ 		printf("perform sub if statement file");
+ 		}			
+     }  
+	else
+    {
+ 	 if(data==10)
+ 		{
+ 		printf("perform else part if");
+ 		}
+ 	  else
+ 		{
+ 			printf("perform else part else");
+ 		}
+ 	} */
+//------------------------------	
+	/*if( data==10) //If ladder
+       {
+        printf("first check of if");
+       } 
+       else if( data==100)
+ 		{
+ 			printf("2 check but else if part");
+ 		}
+ 		else if(data==101)
+ 		{
+ 		printf("2 check but else if part");
+ 		}
+   else 
+    {
+     printf("default");
+    }*/
+//------------------------------	
+	
+	
+	
+	
+	
+	
+	
+return 0;
+}
+
